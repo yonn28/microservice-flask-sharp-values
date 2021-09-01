5ef7c24008160e1bac52f57e69ed713880785ffb
namonroyr <namonroyr@unal.edu.co>
2021-08-31 21:02:47 -0500

    json.dumps

--- index.py
+++ index.py
@@ -6,6 +6,7 @@ from flask_sslify import SSLify
 from urllib.request import urlopen
 import joblib
 import ssl
+import json
 ssl._create_default_https_context = ssl._create_unverified_context
 
 app = Flask(__name__)
@@ -41,12 +42,12 @@ def createTable_top(objeto_modelo, base_variables):
 
     c_df = base_variables[base_variables["Range_probability"] == buscado]
     c_df = c_df[['MIN_ZScorePesoTalla_12M', 'AVG_ZScorePesoTalla_12M', 'MAX_ZScorePesoTalla_12M', 'Veces_DesnutricionSM_12M',
-        'Veces_SobrePeso_12M', 'Veces_Normal_12M', 'TienePasado', 'Veces_Normal_12M', 'sexo_persona_1.0',
+        'Veces_SobrePeso_12M', 'Veces_Normal_12M', 'TienePasado', 'sexo_persona_1.0',
         'tip_cuidado_niños_2.0', 'tip_cuidado_niños_3.0', 'tip_cuidado_niños_4.0', 'tip_cuidado_niños_5.0',
         'tip_cuidado_niños_6.0', 'tip_cuidado_niños_7.0', 'tip_cuidado_niños_8.0', 'tip_cuidado_niños_9.0',
         'ingresos_promp_imp', 'uni_dias_agua', 'cod_clase_2.0', 'cod_clase_3.0', 'noprivaciones', 'ind_estudia_1.0',
         'estrato_1.0', 'estrato_2.0', 'estrato_3.0', 'estrato_4.0', 'estrato_5.0', 'estrato_6.0', 'Probability',
-        'Range_probability']]
+        'Range_probability']].copy()
 
     return (c_df)
 
@@ -57,15 +58,15 @@ top10_rel = createTable_top(modelo_relapse, base_relapse)
 def getting_dataframe_mal():
     top10_mal["Range_probability"] = top10_mal["Range_probability"].astype(str)
     # print(top10_mal)
-    return jsonify(top10_mal.to_dict("records"))
+    return json.dumps(top10_mal.to_dict("records"))
 
 @app.route('/api/v2/rel', methods=['GET'])
 def getting_dataframe_rel():
     top10_rel["Range_probability"] = top10_rel["Range_probability"].astype(str)
     # print(top10_mal)
-    return jsonify(top10_rel.to_dict("records"))
+    return json.dumps(top10_rel.to_dict("records"))
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host = '0.0.0.0',port=8080) #change port to 8080 for deployment, and host = '0.0.0.0'
+    app.run(debug=True, host='0.0.0.0', port=8080) #change port to 8080 for deployment, and host = '0.0.0.0'
     # app.run(debug=True, port=3000) #change port to 8080 for deployment, and host = '0.0.0.0'
