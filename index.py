1e370b95b14f7b915e46b4798346e3c25ac46efb
namonroyr <namonroyr@unal.edu.co>
2021-09-01 20:41:40 -0500

    rev taking out intital-end

--- index.py
+++ index.py
@@ -41,10 +41,6 @@ s_table_m = functions.table_to_show(top10_df_m)
 show_table_m = s_table_m[s_table_m['AVG ZScore'] > -100].sample(frac=0.2)
 
 
-"""
-
-"""
-
 @app.route('/api/v2/mal_n', methods=['GET'])
 def getting_dataframe_mal_n():
     return jsonify(n_children_m)
@@ -71,15 +67,15 @@ def getting_dataframe_shap_rel():
 
 @app.route('/api/v2/show_rel', methods=['GET'])
 def getting_dataframe_rel():
-    #initial = int(request.headers.get('initial'))
-    #end = int(request.headers.get('end'))
-    return jsonify(show_table_r.to_dict("records"))
+    initial = int(request.headers.get('initial'))
+    end = int(request.headers.get('end'))
+    return jsonify(show_table_r[initial:end].to_dict("records"))
 
 @app.route('/api/v2/show_mal', methods=['GET'])
 def getting_dataframe_mal():
-    #initial = int(request.headers.get('initial'))
-    #end = int(request.headers.get('end'))
-    return jsonify(show_table_m.to_dict("records"))
+    initial = int(request.headers.get('initial'))
+    end = int(request.headers.get('end'))
+    return jsonify(show_table_m[initial:end].to_dict("records"))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080) #change port to 8080 for deployment, and host = '0.0.0.0'
